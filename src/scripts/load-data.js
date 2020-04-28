60eca7ce0dc34d56ec580c482089c84370ed1460
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-04-28 17:47:55 +0200

    removing button after click

--- src/scripts/load-data.js
+++ src/scripts/load-data.js
@@ -48,6 +48,7 @@ const loadData = () => {
         else{
         newProducts = newProducts + '<div class="shop-card"><img class="shop-card__img" src="' + item.image + '"alt="Card image cap"><div class="card-body"><p class="card-body__item">' + item.text  + '</p><p class="card-body__item--price">' + item.price + '</p><p class="card-body__item--add">ADD TO CART</p><p class="card-body__item--icons"><img class="card-body__img" src="/assets/9ef4929d09df29ff3a001987ec297b1c.svg" alt="search"><img class="card-body__img" src="/assets/a2525626f2e8ee6fa6faa440a1d8ec0c.svg" alt="heart"></p></div></div>';
         }
+        i++;
     });
     newProducts = newProducts + '</div>';
     shop.innerHTML = shopOld + newProducts;
